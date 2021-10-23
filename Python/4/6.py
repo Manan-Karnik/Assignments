18afa7a3c5960dd4200fcb6938a4eda3c2116547
Manan Karnik <manankarnik03@gmail.com>
2021-10-23 16:02:40 +0530

    Update 6.py

--- Python/4/6.py
+++ Python/4/6.py
@@ -2,4 +2,4 @@
 ip_str = input("Enter a string: ")
 
 # Print the length of input string
-print("The length of the '{0}' is {1}".format(ip_str, len(ip_str)))
+print("The length of '{0}' is {1}".format(ip_str, len(ip_str)))
