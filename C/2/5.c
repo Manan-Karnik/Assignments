43799c5485028482458b7bfafd34c11af90e9e46
Manan Karnik <manankarnik03@gmail.com>
2021-10-28 13:25:52 +0530

    Add files via upload

--- C/2/5.c
+++ C/2/5.c
@@ -7,6 +7,11 @@ int main()
     printf("Enter a year: ");
     scanf("%d", &year);
 
+    /*
+    Condition for leap year:
+        1. The year is multiple of 400
+        2. The year is multiple of 4 and not multiple of 100
+    */
     if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
     {
         printf("%d is a leap year", year);
